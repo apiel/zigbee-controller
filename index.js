2180ad3689132645db74aa8c6310f7267a8e3f84
Alexandre Piel <alexandre.piel@gmail.com>
2020-07-02 23:10:47 +0200

    onMsg

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-const Controller = require('zigbee2mqtt/lib/controller');
+const Controller = require("zigbee2mqtt/lib/controller");
 const controller = new Controller();
 
 const mqtt = {
-    on: (name, value) => { console.log('on', { name, value }); },
-    connect: async() => {},
-    disconnect: async() => {},
-    publish: async(topic, payload, ...rest) => { console.log('pub', { topic, payload, rest }); },
-    subscribe: (topic) => { console.log('sub', topic); },
-    isConnected: () => true,
+  on: (name, value) => {
+    console.log("on", { name, value });
+  },
+  connect: async () => {},
+  disconnect: async () => {},
+  publish: async (topic, payload, ...rest) => {
+    console.log("pub", { topic, payload, rest });
+  },
+  subscribe: (topic) => {
+    console.log("sub", topic);
+  },
+  isConnected: () => true,
+  onMessage: (topic, message) => {
+    console.log("onMessage", { topic, message });
+  },
 };
 
 controller.mqtt = mqtt;
